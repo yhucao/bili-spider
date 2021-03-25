dbf562053219d432b67d3b2a653aeca9f1db6620
Uriah Cao <yhucao@pm.me>
2021-03-25 16:35:34 +0800

    Update bili_video_info.py

--- bili/bili_video_info.py
+++ bili/bili_video_info.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
 import threading
 import time
 from concurrent import futures
